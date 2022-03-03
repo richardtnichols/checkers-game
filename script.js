ab2cf23f2c492c532a5c87827b518a54f1febed3
richardtnichols <r.nichols1010@gmail.com>
2022-03-03 16:34:55 -0600

    finished ui

--- script.js
+++ script.js
@@ -15,59 +15,39 @@ function convertCPtoString(cM) {
   };
 };
 
-function doGame(playerMove, computerMove) {
-  if (playerMove === 'rock') {
-    switch (computerMove) {
-      case 'rock':
-      ++tieCount;
-      return 'Tie!';
-      case 'paper':
-      ++computerScore;
-      return 'You lose!';
-      case 'scissors':
-      ++playerScore;
-      return 'You win!';
+function checkGameOver(computerScore, playerScore, playCount) {
+  if (playCount === 5) {
+    if (computerScore > playerScore) {
+      title.style.fontSize = '1em';
+      title.innerHTML = 'Computer wins!';
+    }
+    else if (computerScore < playerScore) {
+      title.style.fontSize = '1em';
+      title.innerHTML = 'Player wins!';
+    }
+    else if (computerScore === playerScore) {
+      title.style.fontSize = '1em';
+      title.innerHTML = 'Tie!';
+    }
+    else {
+      return;
     }
   }
-  if (playerMove === 'paper') {
-    switch (computerMove) {
-      case 'rock':
-      ++playerScore  
-      return 'You win!'
-      case 'paper':
-      ++tieCount  
-      return 'Tie!'
-      case 'scissors':
-      ++computerScore  
-      return 'You lose!'
-    }
-  }
-  if (playerMove === 'scissors') {
-    switch (computerMove) {
-      case 'rock':
-      ++computerScore;
-      return 'You lose!';
-      case 'paper':
-      ++playerScore;
-      return 'You win!';
-      case 'scissors':
-      ++tieCount;
-      return 'Tie!';
-    };
-  };
-};
-
-let computerScore = 0;
-let playerScore = 0;
-let tieCount = 0;
-let playerMove = '';
-
+}
 const buttons = document.querySelectorAll('button');
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 const computerScoreTotal = document.querySelector('#computerScoreTotal');
 const playerScoreTotal = document.querySelector('#playerScoreTotal');
-const scissors = document.querySelector('#scissors');
+const tieCount = document.querySelector('#tieCount');
+const title = document.querySelector('h1');
+
+let computerScore = 0;
+let playerScore = 0;
+let tieTotal = 0;
+let playerMove = '';
+let playCount = 0;
 
 buttons.forEach((button) => {
   button.addEventListener('mouseover', () => {
@@ -81,48 +61,62 @@ buttons.forEach((button) => {
 rock.addEventListener('click', () => {
    playerMove = 'rock';
    computerMove = convertCPtoString(parseInt(computerPlay(0, 3)));
+   
    if (computerMove === 'paper') {
      ++computerScore;
+   }
+   else if (computerMove === 'rock') {
+     ++tieTotal;
    }
    else if (computerMove === 'scissors') {
      ++playerScore;
    };
+   
    computerScoreTotal.textContent = computerScore;
    playerScoreTotal.textContent = playerScore;
+   tieCount.textContent = tieTotal;
+   ++playCount;
+   checkGameOver(computerScore, playerScore, playCount)
 });
 
 paper.addEventListener('click', () => {
   playerMove = 'paper';
   computerMove = convertCPtoString(parseInt(computerPlay(0, 3)));
+  
   if (computerMove === 'rock') {
     ++playerScore;
+  }
+  else if (computerMove === 'paper') {
+    ++tieTotal;
   }
   else if (computerMove === 'scissors') {
     ++computerScore;
   };
+  
   computerScoreTotal.textContent = computerScore;
   playerScoreTotal.textContent = playerScore;
+  tieCount.textContent = tieTotal;
+  ++playCount;
+  checkGameOver(computerScore, playerScore, playCount)
 });
 
 scissors.addEventListener('click', () => {
   playerMove = 'scissors';
   computerMove = convertCPtoString(parseInt(computerPlay(0, 3)));
+  
   if (computerMove === 'rock') {
     ++computerScore;
+  }
+  else if (computerMove === 'scissors') {
+    ++tieTotal;
   }
   else if (computerMove === 'paper') {
     ++playerScore;
   };
+  
   computerScoreTotal.textContent = computerScore;
   playerScoreTotal.textContent = playerScore;
+  tieCount.textContent = tieTotal;
+  ++playCount;
+  checkGameOver(computerScore, playerScore, playCount)
 });
-
-/* for (var x = 0; x < 5; ++x) {
-  let playerMove = getPlayerMove()
-  let computerMove = convertCPtoString(parseInt(computerPlay(0, 3)))
-
-  console.log(`You played ${playerMove}`)
-  console.log(`Computer played ${computerMove}`)
-  console.log(doGame(playerMove, computerMove))
-  console.log(`The score is:\nPlayer: ${playerScore}\nComputer: ${computerScore}`)
-}; */
